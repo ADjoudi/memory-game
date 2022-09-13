9e048bcac0508223f5888950fbe3e27149bd1386
Abdelouahab <ad.dev.main@gmail.com>
2022-09-13 18:51:42 +0100

    create layout with scaling

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,50 @@
-import logo from "./logo.svg";
 import "../css/App.css";
 
 function App() {
+  const cards = [];
+  for (let i = 1; i <= 8; i++) {
+    let gridStyle = "gridArea: " + i;
+    cards.push(
+      <img
+        key={i}
+        id={"card" + i}
+        src={require("../assets/card/" + i + ".jpg")}
+        alt={i}
+        style={{ gridStyle }}
+      />
+    );
+  }
+  //TO-DO: make this a state and apply dynamic changes to it.
+  const score = (
+    <img
+      id="currentScore"
+      src={require("../assets/nbr/" + 0 + ".png")}
+      alt={"0"}
+    />
+  );
+  const bestScore = (
+    <img
+      id="bestScore"
+      src={require("../assets/nbr/" + 0 + ".png")}
+      alt={"0"}
+    />
+  );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <img src={require("../assets/logo.png")} alt="Memory Game" />
       </header>
+      <div className="board">{cards}</div>
+      <div className="score">
+        <img src={require("../assets/SCORE.png")} alt="score" />
+        {score}
+        <div className="bestScore">
+          <img src={require("../assets/best.png")} alt="best score" />
+          {/* placeholder */}
+          {bestScore}
+        </div>
+      </div>
     </div>
   );
 }
